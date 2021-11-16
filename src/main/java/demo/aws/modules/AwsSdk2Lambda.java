0b45fe01525d3e791efba86b2a5d56f257b74738
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-16 10:42:15 +0900

    Add invokeFunction

--- src/main/java/demo/aws/modules/AwsSdk2Lambda.java
+++ src/main/java/demo/aws/modules/AwsSdk2Lambda.java
@@ -3,9 +3,12 @@ package demo.aws.modules;
 import java.util.List;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.lambda.LambdaClient;
 import software.amazon.awssdk.services.lambda.model.FunctionConfiguration;
+import software.amazon.awssdk.services.lambda.model.InvokeRequest;
+import software.amazon.awssdk.services.lambda.model.InvokeResponse;
 import software.amazon.awssdk.services.lambda.model.LambdaException;
 import software.amazon.awssdk.services.lambda.model.ListFunctionsResponse;
 
@@ -24,6 +27,7 @@ public class AwsSdk2Lambda {
 	public void list() {
 
         try {
+        	
             ListFunctionsResponse functionResult = client.listFunctions();
             List<FunctionConfiguration> list = functionResult.functions();
 
@@ -37,6 +41,31 @@ public class AwsSdk2Lambda {
         }
         
 	}
+	
+    public void invokeFunction(String functionName) {
+
+       InvokeResponse res = null ;
+       try {
+  
+           //Need a SdkBytes instance for the payload
+           String json = "{\"Hello \":\"Paris\"}";
+           SdkBytes payload = SdkBytes.fromUtf8String(json) ;
+
+           //Setup an InvokeRequest
+           InvokeRequest request = InvokeRequest.builder()
+                   .functionName(functionName)
+                   .payload(payload)
+                   .build();
+
+           res = client.invoke(request);
+           String value = res.payload().asUtf8String() ;
+           System.out.println(value);
+
+       } catch(LambdaException e) {
+           System.err.println(e.getMessage());
+           System.exit(1);
+       }
+   }
 	
 	
 }
