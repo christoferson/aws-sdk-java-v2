6e3ce5335f6df8a0edee3775ccf11923945de64a
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-11-23 01:12:47 +0900

    Add SendRawEmail

--- src/main/java/demo/aws/modules/AwsSdk2Ses.java
+++ src/main/java/demo/aws/modules/AwsSdk2Ses.java
@@ -1,13 +1,31 @@
 package demo.aws.modules;
 
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.Properties;
+
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ses.SesClient;
 import software.amazon.awssdk.services.ses.model.Body;
 import software.amazon.awssdk.services.ses.model.Content;
 import software.amazon.awssdk.services.ses.model.Destination;
+import software.amazon.awssdk.services.ses.model.RawMessage;
 import software.amazon.awssdk.services.ses.model.SendEmailRequest;
 import software.amazon.awssdk.services.ses.model.SendEmailResponse;
+import software.amazon.awssdk.services.ses.model.SendRawEmailRequest;
+import software.amazon.awssdk.services.ses.model.SesException;
 
 public class AwsSdk2Ses {
 
@@ -36,5 +54,103 @@ public class AwsSdk2Ses {
 		
 		System.out.println("sent " + response.messageId());
 	}
+	
+    public  void sendRawEmail(String sender, String recipient) throws IOException {
+
+
+        String subject = "Test Subject";
+
+        // The email body for non-HTML email clients
+        String bodyText = "Hello,\r\n" + "See the list of customers. ";
+
+        // The HTML body of the email
+        String bodyHTML = "<html>" + "<head></head>" + "<body>" + "<h1>Hello!</h1>"
+                + "<p> See the list of customers.</p>" + "</body>" + "</html>";
+
+	    try {
+	         sendRawEmail(client, sender, recipient, subject, bodyText, bodyHTML);
+	         client.close();
+	         System.out.println("Done");
+	
+	    } catch (IOException | MessagingException e) {
+	        e.getStackTrace();
+	    }
+
+
+  }
+
+
+    public  void sendRawEmail(SesClient client,
+                            String sender,
+                            String recipient,
+                            String subject,
+                            String bodyText,
+                            String bodyHTML
+                            ) throws AddressException, MessagingException, IOException {
+
+        Session session = Session.getDefaultInstance(new Properties());
+        MimeMessage message = new MimeMessage(session);
+
+        // Add subject, from and to lines
+        message.setSubject(subject, "UTF-8");
+        message.setFrom(new InternetAddress(sender));
+        message.setRecipients(Message.RecipientType.TO, InternetAddress.parse(recipient));
+
+        // Create a multipart/alternative child container
+        MimeMultipart msgBody = new MimeMultipart("alternative");
+
+        // Create a wrapper for the HTML and text parts
+        MimeBodyPart wrap = new MimeBodyPart();
+
+        // Define the text part
+        MimeBodyPart textPart = new MimeBodyPart();
+        textPart.setContent(bodyText, "text/plain; charset=UTF-8");
+
+        // Define the HTML part
+        MimeBodyPart htmlPart = new MimeBodyPart();
+        htmlPart.setContent(bodyHTML, "text/html; charset=UTF-8");
+
+        // Add the text and HTML parts to the child container
+        msgBody.addBodyPart(textPart);
+        msgBody.addBodyPart(htmlPart);
+
+        // Add the child container to the wrapper object
+        wrap.setContent(msgBody);
+
+        // Create a multipart/mixed parent container
+        MimeMultipart msg = new MimeMultipart("mixed");
+
+        // Add the parent container to the message
+        message.setContent(msg);
+
+        // Add the multipart/alternative part to the message
+        msg.addBodyPart(wrap);
+
+        try {
+            System.out.println("Attempting to send an email through Amazon SES " + "using the AWS SDK for Java...");
+
+             ByteArrayOutputStream outputStream = new ByteArrayOutputStream();
+             message.writeTo(outputStream);
+             ByteBuffer buf = ByteBuffer.wrap(outputStream.toByteArray());
+
+             byte[] arr = new byte[buf.remaining()];
+             buf.get(arr);
+
+             SdkBytes data = SdkBytes.fromByteArray(arr);
+             RawMessage rawMessage = RawMessage.builder()
+                    .data(data)
+                    .build();
+
+             SendRawEmailRequest rawEmailRequest = SendRawEmailRequest.builder()
+                    .rawMessage(rawMessage)
+                    .build();
+
+             client.sendRawEmail(rawEmailRequest);
+
+         } catch (SesException e) {
+            System.err.println(e.awsErrorDetails().errorMessage());
+         }
+
+    }
 	
 }
