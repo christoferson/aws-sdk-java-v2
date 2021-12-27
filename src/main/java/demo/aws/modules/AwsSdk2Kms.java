d912b276879a5bfea6cf111b3ae73c013e4d5b46
christoferson <sweetsorrow1981@yahoo.co.jp>
2021-12-27 18:57:45 +0900

    Add aliasList

--- src/main/java/demo/aws/modules/AwsSdk2Kms.java
+++ src/main/java/demo/aws/modules/AwsSdk2Kms.java
@@ -5,8 +5,11 @@ import java.util.List;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.kms.KmsClient;
+import software.amazon.awssdk.services.kms.model.AliasListEntry;
 import software.amazon.awssdk.services.kms.model.KeyListEntry;
 import software.amazon.awssdk.services.kms.model.KmsException;
+import software.amazon.awssdk.services.kms.model.ListAliasesRequest;
+import software.amazon.awssdk.services.kms.model.ListAliasesResponse;
 import software.amazon.awssdk.services.kms.model.ListKeysRequest;
 import software.amazon.awssdk.services.kms.model.ListKeysResponse;
 
@@ -34,6 +37,20 @@ public class AwsSdk2Kms {
             System.out.println(String.format("%s %s", key.keyArn(), key.keyId()));
         }
 
-      }	
-	
+    }	
+
+    public void aliasList() throws KmsException {
+
+        ListAliasesRequest aliasesRequest = ListAliasesRequest.builder()
+            .limit(15)
+            .build();
+
+        ListAliasesResponse aliasesResponse = client.listAliases(aliasesRequest) ;
+        List<AliasListEntry> aliases = aliasesResponse.aliases();
+
+        for (AliasListEntry alias: aliases) {
+            System.out.println(String.format("%s %s", alias.aliasName(), alias.targetKeyId()));
+        }
+
+    }
 }
