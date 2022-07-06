87e2e288818f4be9658cdfcea04d2fa20ec31e61
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-07-06 13:46:21 +0900

    AWS SDK2 - ApiGateway - Add apiKeyCreate

--- src/main/java/demo/aws/modules/AwsSdk2ApiGateway.java
+++ src/main/java/demo/aws/modules/AwsSdk2ApiGateway.java
@@ -6,6 +6,10 @@ import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.apigateway.ApiGatewayClient;
 import software.amazon.awssdk.services.apigateway.model.ApiKey;
 import software.amazon.awssdk.services.apigateway.model.ApiStage;
+import software.amazon.awssdk.services.apigateway.model.CreateApiKeyRequest;
+import software.amazon.awssdk.services.apigateway.model.CreateApiKeyResponse;
+import software.amazon.awssdk.services.apigateway.model.CreateUsagePlanKeyRequest;
+import software.amazon.awssdk.services.apigateway.model.CreateUsagePlanKeyResponse;
 import software.amazon.awssdk.services.apigateway.model.GetApiKeyRequest;
 import software.amazon.awssdk.services.apigateway.model.GetApiKeyResponse;
 import software.amazon.awssdk.services.apigateway.model.GetApiKeysRequest;
@@ -16,12 +20,16 @@ import software.amazon.awssdk.services.apigateway.model.GetUsagePlansRequest;
 import software.amazon.awssdk.services.apigateway.model.GetUsagePlansResponse;
 import software.amazon.awssdk.services.apigateway.model.UsagePlan;
 import software.amazon.awssdk.services.apigateway.model.UsagePlanKey;
+
+// https://sdk.amazonaws.com/java/api/latest/software/amazon/awssdk/services/apigateway/ApiGatewayClient.html#createUsagePlanKey--
 // https://docs.aws.amazon.com/apigateway/latest/developerguide/api-gateway-api-usage-plans.html?icmpid=docs_apigateway_console
 // A usage plan specifies who can access one or more deployed API stages and methods—and optionally sets the target request rate to start throttling requests.
 //  The plan uses API keys to identify API clients and who can access the associated API stages for each key.
 // API keys are alphanumeric string values that you distribute to application developer customers to grant access to your API. 
 // An API key can be associated with more than one usage plan. A usage plan can be associated with more than one stage. 
 //  However, a given API key can only be associated with one usage plan for each stage of your API.
+// 	createUsagePlanKey(CreateUsagePlanKeyRequest createUsagePlanKeyRequest)
+// updateUsagePlan(UpdateUsagePlanRequest updateUsagePlanRequest)
 public class AwsSdk2ApiGateway {
 
 	private ApiGatewayClient client;
@@ -69,6 +77,23 @@ public class AwsSdk2ApiGateway {
 
 	}
 	
+	
+	public void apiKeyCreate(String name, String description) {
+
+		System.out.println(String.format("Create ApiKey"));
+		
+		CreateApiKeyRequest request = CreateApiKeyRequest.builder()
+				.name(name)
+				.description(description)
+				.build();
+
+		CreateApiKeyResponse result = client.createApiKey(request);
+		CreateApiKeyResponse apiKey = result;
+		System.out.println(String.format("ID=%s Name=%s Description=%s CustomerId=%s", 
+				apiKey.id(), apiKey.name(), apiKey.description(), apiKey.customerId()));
+
+	}
+	
 	public void usagePlanList() {
 
 		System.out.println(String.format("List UsagePlan"));
@@ -88,6 +113,5 @@ public class AwsSdk2ApiGateway {
 		}
 
 	}
-
 	
 }
