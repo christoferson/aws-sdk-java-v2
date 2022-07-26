c7307dba731fb81250928e434fe2517f5d011380
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-07-26 22:00:03 +0900

    AWSSDK2 - Add Secrets Manager

---
+++ src/main/java/demo/aws/modules/AwsSdk2SecretsManager.java
@@ -0,0 +1,44 @@
+package demo.aws.modules;
+
+import java.util.List;
+import java.util.Objects;
+
+
+import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
+import software.amazon.awssdk.core.SdkBytes;
+import software.amazon.awssdk.regions.Region;
+
+import software.amazon.awssdk.services.secretsmanager.SecretsManagerClient;
+import software.amazon.awssdk.services.secretsmanager.model.ListSecretsRequest;
+import software.amazon.awssdk.services.secretsmanager.model.ListSecretsResponse;
+import software.amazon.awssdk.services.secretsmanager.model.SecretListEntry;
+
+public class AwsSdk2SecretsManager {
+
+	private SecretsManagerClient client;
+	
+	public AwsSdk2SecretsManager(AwsCredentialsProvider credentialsProvider, Region region) {
+		
+		this.client = SecretsManagerClient.builder()
+				  .credentialsProvider(credentialsProvider)
+				  .region(region)
+				  .build();
+	}
+	
+    public void secretList() {
+
+    	System.out.printf("Listing Secrets ... %n");
+    	
+    	ListSecretsRequest request = ListSecretsRequest.builder()
+                .build();
+
+    	ListSecretsResponse response = client.listSecrets(request);
+        List<SecretListEntry> list = response.secretList();
+        for (SecretListEntry element : list) {
+            System.out.println(String.format("%s %s", element.arn(), element.description()));
+        }
+
+    }	
+
+
+}
