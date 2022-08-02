19b23fb2a8a536c176f7059520c2c972a1ceab6c
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-08-02 23:05:16 +0900

    AWS SDK2 - RDS - Add instanceList

--- src/main/java/demo/aws/modules/AwsSdk2Rds.java
+++ src/main/java/demo/aws/modules/AwsSdk2Rds.java
@@ -6,7 +6,13 @@ import java.util.List;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.kms.model.KeyListEntry;
+import software.amazon.awssdk.services.kms.model.ListKeysRequest;
+import software.amazon.awssdk.services.kms.model.ListKeysResponse;
 import software.amazon.awssdk.services.rds.RdsClient;
+import software.amazon.awssdk.services.rds.model.DBInstance;
+import software.amazon.awssdk.services.rds.model.DescribeDbInstancesRequest;
+import software.amazon.awssdk.services.rds.model.DescribeDbInstancesResponse;
 import software.amazon.awssdk.services.xray.XRayClient;
 import software.amazon.awssdk.services.xray.model.GetGroupsResponse;
 import software.amazon.awssdk.services.xray.model.GetServiceGraphRequest;
@@ -29,5 +35,23 @@ public class AwsSdk2Rds {
 				  .region(region)
 				  .build();
 	}
+	
+	public void instanceList() {
+
+    	System.out.printf("List Database Instance ... %n");
+    	
+    	DescribeDbInstancesRequest request = DescribeDbInstancesRequest.builder()
+                .build();
+
+    	DescribeDbInstancesResponse response = client.describeDBInstances(request);
+        List<DBInstance> elements = response.dbInstances();
+        for (DBInstance element : elements) {
+            System.out.println(String.format("Arn=%s Name=%s Engine=%s EngineVer=%s Endpoint=%s MultiAz=%s", element.dbInstanceArn(), element.dbName(), element.engine(), 
+            		element.engineVersion(), element.endpoint().address(),
+            		element.multiAZ()));
+        }
+        
+	}
+
 	
 }
