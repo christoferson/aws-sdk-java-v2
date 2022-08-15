deef13f33831ecbc5b970fbddd7cb17db0c5a4a8
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-08-15 16:13:15 +0900

    Add AwsClientDaxAsync - Item Get

--- src/main/java/demo/aws/client/dax/AwsClientDaxAsync.java
+++ src/main/java/demo/aws/client/dax/AwsClientDaxAsync.java
@@ -4,14 +4,26 @@ import software.amazon.dax.ClusterDaxAsyncClient;
 import software.amazon.dax.Configuration;
 
 import java.io.IOException;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.dynamodb.DynamoDbAsyncClient;
+import software.amazon.awssdk.services.dynamodb.model.AttributeValue;
+import software.amazon.awssdk.services.dynamodb.model.GetItemRequest;
+import software.amazon.awssdk.services.dynamodb.model.GetItemResponse;
 
 
 public class AwsClientDaxAsync {
 
+    private static AttributeValue attr(String s) {
+        return AttributeValue.builder().s(s).build();
+    }
+    
 	private DynamoDbAsyncClient client;
 	
 	public AwsClientDaxAsync(AwsCredentialsProvider credentialsProvider, Region region, String daxUrl) {
@@ -28,5 +40,25 @@ public class AwsClientDaxAsync {
 		}
 	}
 
+	public void itemGet(String tableName, 
+			String partitionKeyName, String partitionKey, 
+			String sortKeyName, String sortKey) {
+		
+		try {
+			CompletableFuture<GetItemResponse> response = client.getItem(GetItemRequest.builder()
+			        .tableName(tableName)
+			        .key(Map.of(partitionKeyName, attr(partitionKey), sortKeyName, attr(sortKey)))
+			        .build());
+			
+			response.get(5, TimeUnit.SECONDS);
+		} catch (InterruptedException e) {
+			throw new RuntimeException(e);
+		} catch (ExecutionException e) {
+			throw new RuntimeException(e);
+		} catch (TimeoutException e) {
+			throw new RuntimeException(e);
+		}
+		
+	}
 	
 }
