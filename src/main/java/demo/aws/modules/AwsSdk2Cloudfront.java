a9e0aa7abb57372b4971599b1a0fe666df39f154
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-09-05 00:19:04 +0900

    AWS SDK2 - Cloudfront - distributionList

--- src/main/java/demo/aws/modules/AwsSdk2Cloudfront.java
+++ src/main/java/demo/aws/modules/AwsSdk2Cloudfront.java
@@ -1,9 +1,13 @@
 package demo.aws.modules;
 
+import java.util.List;
+
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
-
 import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
+import software.amazon.awssdk.services.cloudfront.model.DistributionSummary;
+import software.amazon.awssdk.services.cloudfront.model.ListDistributionsRequest;
+import software.amazon.awssdk.services.cloudfront.model.ListDistributionsResponse;
 
 public class AwsSdk2Cloudfront {
 
@@ -13,9 +17,23 @@ public class AwsSdk2Cloudfront {
 		
 		this.client = CloudFrontClient.builder()
 				  .credentialsProvider(credentialsProvider)
-				  .region(region)
+				  .region(Region.AWS_GLOBAL)
 				  .build();
 	}
 
+	public void distributionList() {
+
+		System.out.println(String.format("List Cloudfront Distribution"));
+		
+		ListDistributionsRequest request = ListDistributionsRequest.builder()
+				.build();
+
+		ListDistributionsResponse result = client.listDistributions(request);
+		List<DistributionSummary> elements = result.distributionList().items();
+		for (DistributionSummary element : elements) {
+			System.out.println(String.format("ID=%s Comment=%s DomainName=%s", 
+					element.id(), element.comment(), element.domainName()));
+		}
+	}
 	
 }
