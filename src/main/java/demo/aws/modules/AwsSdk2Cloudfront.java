260011f0d675cfa5ad1ad7e3afc67fef62865b14
christoferson <sweetsorrow1981@yahoo.co.jp>
2022-09-05 23:52:48 +0900

    Add originAccessIdentityList

--- src/main/java/demo/aws/modules/AwsSdk2Cloudfront.java
+++ src/main/java/demo/aws/modules/AwsSdk2Cloudfront.java
@@ -5,7 +5,10 @@ import java.util.List;
 import software.amazon.awssdk.auth.credentials.AwsCredentialsProvider;
 import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.cloudfront.CloudFrontClient;
+import software.amazon.awssdk.services.cloudfront.model.CloudFrontOriginAccessIdentitySummary;
 import software.amazon.awssdk.services.cloudfront.model.DistributionSummary;
+import software.amazon.awssdk.services.cloudfront.model.ListCloudFrontOriginAccessIdentitiesRequest;
+import software.amazon.awssdk.services.cloudfront.model.ListCloudFrontOriginAccessIdentitiesResponse;
 import software.amazon.awssdk.services.cloudfront.model.ListDistributionsRequest;
 import software.amazon.awssdk.services.cloudfront.model.ListDistributionsResponse;
 
@@ -33,6 +36,21 @@ public class AwsSdk2Cloudfront {
 		for (DistributionSummary element : elements) {
 			System.out.println(String.format("ID=%s Comment=%s DomainName=%s", 
 					element.id(), element.comment(), element.domainName()));
+		}
+	}
+	
+	public void originAccessIdentityList() {
+
+		System.out.println(String.format("List CloudFrontOriginAccessIdentities"));
+		
+		ListCloudFrontOriginAccessIdentitiesRequest request = ListCloudFrontOriginAccessIdentitiesRequest.builder()
+				.build();
+
+		ListCloudFrontOriginAccessIdentitiesResponse result = client.listCloudFrontOriginAccessIdentities(request);
+		List<CloudFrontOriginAccessIdentitySummary> elements = result.cloudFrontOriginAccessIdentityList().items();
+		for (CloudFrontOriginAccessIdentitySummary element : elements) {
+			System.out.println(String.format("ID=%s Comment=%s CanonicalUserId=%s", 
+					element.id(), element.comment(), element.s3CanonicalUserId()));
 		}
 	}
 	
